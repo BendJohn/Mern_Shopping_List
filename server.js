e93f297bae4f46dfb7fefe1347fdbda640ec7c73
BendJohn <42980091+BendJohn@users.noreply.github.com>
2020-05-19 21:15:37 -0500

    preparing from deployment

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');       // Deals with file paths
 
 const items = require('./routes/api/items');
 
@@ -22,6 +23,16 @@ mongoose
 // Use Routes - anything that goes to /api/items should refer to the file ^^^
 app.use('/api/items', items);
 
-// Create variable for port you're going to use. Specifically for Heroku
+// Serve static assets (in build) if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+// Create variable for port you're going to use. For non-Heroku testing, use port 5000
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
