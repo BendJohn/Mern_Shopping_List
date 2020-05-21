895f1e66351f2a5756ab71fe1b261033d4ef5ee8
BendJohn <42980091+BendJohn@users.noreply.github.com>
2020-05-20 21:02:45 -0500

    Added auth code

--- config/keys.js
+++
@@ -1,4 +0,0 @@
-// This is our mongo database.
-module.exports = {
-    mongoURI: 'mongodb://ben:ben123@ds231723.mlab.com:31723/mern_shopping'
-}
--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 // Things we need
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const path = require('path');       // Deals with file paths
-
-const items = require('./routes/api/items');
+const config = require('config');
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
 // DB Config
-const db = require('./config/keys').mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
-    .connect(db)
+    .connect(db, { useNewUrlParser: true, useCreateIndex: true })
     .then(() => console.log("MongoDB Connected..."))
     .catch(err => console.log(err))
 
 // Use Routes - anything that goes to /api/items should refer to the file ^^^
-app.use('/api/items', items);
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
 
 // Serve static assets (in build) if in production
 if (process.env.NODE_ENV === 'production') {
